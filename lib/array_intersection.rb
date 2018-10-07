ec2fd520f1e067784aa52275004002d1db299469
Addison Cole <addison.cole85@gmail.com>
2018-10-06 18:58:56 -0700

    add method to determine intersection of two arrays.

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,18 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  int_arr = []
+  if array1 == nil || array2 == nil
+    return int_arr
+  end
+  y = (array1.length - 1)
+  x = 0
+  until x > y do
+    array2.each do |i|
+      if array1[x] == i
+        int_arr << array1[x]
+      end
+    end
+    x += 1
+  end
+  return int_arr
 end
